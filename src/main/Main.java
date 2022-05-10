9efac7224b835920659c2a816bcc496e3d268d2f
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-10 13:49:29 -0400

    chap01- completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.VBox;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
