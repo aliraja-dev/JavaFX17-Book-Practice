3e94930e2ac43317ff0d4aa1a2ce7f9359f3db9f
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-10 11:36:48 -0400

    Root Node Vbox

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -10,9 +14,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // TODO Auto-generated method stub
-
-        primaryStage.setTitle("JavaFX17 Book Practice App");
+        VBox root = new VBox();
+        // root.getChildren().addAll(new Button("Button 1"), new Button("Button 2"), new
+        // Text("Hello from JavaFX17"));
+        Button exitBtn = new Button("Exit");
+        exitBtn.setOnAction(event -> System.exit(0));
+        root.getChildren().add(exitBtn);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("JavaFX17 Book Chap01");
         primaryStage.show();
 
     }
