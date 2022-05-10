8ecce8928d8440a437fbbbafad9b8bab9e8f52c6
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-10 10:23:50 -0400

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+        primaryStage.setTitle("JavaFX17 Book Practice App");
+        primaryStage.show();
+
+    }
+}
