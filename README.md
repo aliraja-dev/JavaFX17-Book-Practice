8b7617421cf9beedaa13e9a68d210b5ad82cf67b
Ali Raja <ali@alis-mac-studio.mynetworksettings.com>
2022-05-11 16:33:32 -0400

    Basic Mouse click Handler added

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
-# JavaFX17 Book - Chap06 Branch - Nodes
+# JavaFX17 Book - Chap09 Branch - Event Handling
 
 This repo is structured to show code examples for chap02. Every example will have its own commit and associated screenshot with same title as the commit message. To view code for a GUI SS, open respective commit.
 
 Practice Code Examples wrt every commit.
-Chap02: Nodes in JavaFX Graph Scene Data Structure
+Chap09: Event Handling
 
 #### Listing 6-7, tooltip using Un-managed Nodes for Micro Text Help
 
-As all managed nodes, are laid out by their parent node. And the parent node needs to figure out their size and position. Therefore if we want to show a tooltip without disturbing the layout of the parent node, we use Un managed nodes, and specify the position and size for the node without bothering its parent.
-As shown below the tool tip text in red for hints doesn't shift the text fields in the parent node.
-
 ![homepage](images/ss-helptext1.png)
-![homepage](images/ss-helptext2.png)
-![homepage](images/ss-helptext3.png)
-
-#### Listing 6-9, Moving Node between Cooridnate Bounds of Nodes
-
-Moving red circle between fields on focus
-
-![homepage](images/ss-movingmarkers1.png)
-![homepage](images/ss-movingmarkers2.png)
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,172 +1,42 @@
-
-// CoordinateConversion.java
+// Main.java
 package main;
 
 import javafx.application.Application;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    // An instance variable to store the reference of the circle
-
-    private Circle marker;
-
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) {
-        TextField fName = new TextField();
-        TextField lName = new TextField();
-        TextField salary = new TextField();
+        Circle circle = new Circle(100, 100, 50);
+        circle.setFill(Color.CORAL);
 
-        // The Circle node is unmanaged
-        marker = new Circle(5);
-        marker.setManaged(false);
-        marker.setFill(Color.RED);
-        marker.setMouseTransparent(true);
+        // Create a MouseEvent filter
+        EventHandler<MouseEvent> mouseEventFilter = e -> System.out.println("Mouse event filter has been called.");
 
-        HBox hb1 = new HBox();
-        HBox hb2 = new HBox();
-        HBox hb3 = new HBox();
-        hb1.getChildren().addAll(new Label("First Name:"), fName);
-        hb2.getChildren().addAll(new Label("Last Name:"), lName);
-        hb3.getChildren().addAll(new Label("Salary:"), salary);
+        // Create a MouseEvent handler
+        EventHandler<MouseEvent> mouseEventHandler = e -> System.out.println("Mouse event handler has been called.");
 
-        VBox root = new VBox();
-        root.getChildren().addAll(hb1, hb2, hb3, marker);
+        // Register the MouseEvent filter and handler to the Circle
+        // for mouse-clicked events
+        circle.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEventFilter);
+        circle.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEventHandler);
 
+        HBox root = new HBox();
+        root.getChildren().add(circle);
         Scene scene = new Scene(root);
-
-        // Add a focus change listener to the scene
-        scene.focusOwnerProperty().addListener(
-                (prop, oldNode, newNode) -> placeMarker(newNode));
-
         stage.setScene(scene);
-        stage.setTitle("Coordinate Space Transformation");
+        stage.setTitle("Registering Event Filters and Handlers");
         stage.show();
-    }
-
-    public void placeMarker(Node newNode) {
-        double nodeMinX = newNode.getLayoutBounds().getMinX();
-        double nodeMinY = newNode.getLayoutBounds().getMinY();
-        Point2D nodeInScene = newNode.localToScene(nodeMinX, nodeMinY);
-        Point2D nodeInMarkerLocal = marker.sceneToLocal(nodeInScene);
-        Point2D nodeInMarkerParent = marker.localToParent(nodeInMarkerLocal);
-
-        // Position the circle approperiately
-        marker.relocate(nodeInMarkerParent.getX()
-                + marker.getLayoutBounds().getMinX(),
-                nodeInMarkerParent.getY()
-                        + marker.getLayoutBounds().getMinY());
+        stage.sizeToScene();
     }
 }
-// // MicroHelpApp.java
-// package main;
-
-// import javafx.application.Application;
-// import javafx.application.Platform;
-// import javafx.beans.value.ObservableValue;
-// import javafx.geometry.VPos;
-// import javafx.scene.Node;
-// import javafx.scene.Scene;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.Label;
-// import javafx.scene.control.TextField;
-// import javafx.scene.layout.GridPane;
-// import javafx.scene.paint.Color;
-// import javafx.scene.text.Font;
-// import javafx.scene.text.Text;
-// import javafx.stage.Stage;
-
-// public class Main extends Application {
-// // An instance variable to store the Text node reference
-// private Text helpText = new Text();
-
-// public static void main(String[] args) {
-// Application.launch(args);
-// }
-
-// @Override
-// public void start(Stage stage) {
-// TextField fName = new TextField();
-// TextField lName = new TextField();
-// TextField salary = new TextField();
-
-// Button closeBtn = new Button("Close");
-// closeBtn.setOnAction(e -> Platform.exit());
-
-// fName.getProperties().put("microHelpText", "Enter the first name");
-// lName.getProperties().put("microHelpText", "Enter the last name");
-// salary.getProperties().put("microHelpText",
-// "Enter a salary greater than $2000.00.");
-
-// // The help text node is unmanaged
-// helpText.setManaged(false);
-// helpText.setTextOrigin(VPos.TOP);
-// helpText.setFill(Color.RED);
-// helpText.setFont(Font.font(null, 9));
-// helpText.setMouseTransparent(true);
-
-// // Add all nodes to a GridPane
-// GridPane root = new GridPane();
-
-// root.add(new Label("First Name:"), 1, 1);
-// root.add(fName, 2, 1);
-// root.add(new Label("Last Name:"), 1, 2);
-// root.add(lName, 2, 2);
-
-// root.add(new Label("Salary:"), 1, 3);
-// root.add(salary, 2, 3);
-// root.add(closeBtn, 3, 3);
-// root.add(helpText, 4, 3);
-
-// Scene scene = new Scene(root, 300, 100);
-
-// // Add a change listener to the scene, so we know when
-// // the focus owner changes and display the micro help
-// scene.focusOwnerProperty().addListener(
-// (ObservableValue<? extends Node> value, Node oldNode, Node newNode) ->
-// focusChanged(value, oldNode,
-// newNode));
-// stage.setScene(scene);
-// stage.setTitle("Showing Micro Help");
-// stage.show();
-// }
-
-// public void focusChanged(ObservableValue<? extends Node> value,
-// Node oldNode, Node newNode) {
-// // Focus has changed to a new node
-// String microHelpText = (String) newNode.getProperties().get("microHelpText");
-
-// if (microHelpText != null && microHelpText.trim().length() > 0) {
-// helpText.setText(microHelpText);
-// helpText.setVisible(true);
-
-// // Position the help text node
-// double x = newNode.getLayoutX() +
-// newNode.getLayoutBounds().getMinX() -
-// helpText.getLayoutBounds().getMinX();
-// double y = newNode.getLayoutY() +
-// newNode.getLayoutBounds().getMinY() +
-// newNode.getLayoutBounds().getHeight() -
-// helpText.getLayoutBounds().getMinX();
-
-// helpText.setLayoutX(x);
-// helpText.setLayoutY(y);
-// helpText.setWrappingWidth(newNode.getLayoutBounds().getWidth());
-// } else {
-// helpText.setVisible(false);
-// }
-// }
-// }
